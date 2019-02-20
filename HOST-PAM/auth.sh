86e5618d6c347ec2d2b111b9b11fca96f165077f
joseoterop <44051345+joseoterop@users.noreply.github.com>
2019-02-20 21:06:40 +0100

    Delete auth.sh

--- HOST-PAM/auth.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-# @edt ASIX-M06
-# -----------------
-authconfig  --enableshadow --enablelocauthorize --enableldap  --ldapserver='ldap' --ldapbase='dc=edt,dc=org' --enableldapauth  --updateall
-
-
-
