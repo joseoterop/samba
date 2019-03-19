252d6f9fd4c94117e07c4143d898fb89431d85ee
joseoterop <44051345+joseoterop@users.noreply.github.com>
2019-03-19 22:34:56 +0100

    Update samba_users.sh

--- SAMBA/samba_users.sh
+++ SAMBA/samba_users.sh
@@ -8,12 +8,12 @@ useradd pla
 useradd patipla
 useradd rock
 
-echo -e "smbpla\nsmbpla" | smbpasswd -a pla
-echo -e "smbpatipla\nsmbpatipla" | smbpasswd -a patipla
-echo -e "smbrock\nsmbrock" | smbpasswd -a rock
-echo -e "smblila\nsmblila" | smbpasswd -a lila
-echo -e "smbpere\nsmbpere" | smbpasswd -a pere
-echo -e "smbpau\nsmbpau" | smbpasswd -a pau
-echo -e "smbmarta\nsmbmarta" | smbpasswd -a marta
+echo -e "smbpla\npla" | smbpasswd -a pla
+echo -e "smbpatipla\npatipla" | smbpasswd -a patipla
+echo -e "smbrock\nrock" | smbpasswd -a rock
+echo -e "smblila\nlila" | smbpasswd -a lila
+echo -e "smbpere\npere" | smbpasswd -a pere
+echo -e "smbpau\npau" | smbpasswd -a pau
+echo -e "smbmarta\nmarta" | smbpasswd -a marta
 
 
